a129e3651a3a98590aa35b10ca4e714f9f26bba9
Igor Veras <igor.veras.daniel@gmail.com>
2025-10-20 20:06:59 -0300

    Segunda tentativa Increasing, sucesso

--- increasing_Triplet_Subsequence/Exercicio2.c
+++ increasing_Triplet_Subsequence/Exercicio2.c
@@ -9,19 +9,16 @@ public:
         int primeiro = std::numeric_limits<int>::max();
         int segundo = std::numeric_limits<int>::max();
 
-        // Esta lógica falha. Ela não atualiza 'primeiro' para um
-        // candidato melhor depois que 'segundo' já foi encontrado.
         for (int num : nums) {
             
-            if (num > segundo) {
-                return true;
-            } 
-            
-            // A ordem aqui está trocada, e a lógica é diferente
-            if (num > primeiro) {
-                segundo = num;
-            } else {
+            if (num <= primeiro) {
                 primeiro = num;
+            } 
+            else if (num <= segundo) {
+                segundo = num;
+            } 
+            else { 
+                return true;
             }
         }
         
